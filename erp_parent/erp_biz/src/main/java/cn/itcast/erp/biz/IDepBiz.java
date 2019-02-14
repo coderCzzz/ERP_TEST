d902d636d956afa9813d5568d29de6215d82f220
caoz <caozhuang0@126.com>
2019-02-14 20:22:14 +0800

    代码生成工具生成后的代码

--- erp_parent/erp_biz/src/main/java/cn/itcast/erp/biz/IDepBiz.java
+++
@@ -1,8 +0,0 @@
-package cn.itcast.erp.biz;
-
-import java.util.List;
-
-import cn.itcast.erp.entity.Dep;
-
-public interface IDepBiz extends IBaseBiz<Dep>{
-}
--- erp_parent/erp_dao/src/main/java/cn/itcast/erp/dao/IDepDao.java
+++
@@ -1,5 +0,0 @@
-package cn.itcast.erp.dao;
-import java.util.List;
-import cn.itcast.erp.entity.Dep;
-public interface IDepDao extends IBaseDao<Dep>{
-}
